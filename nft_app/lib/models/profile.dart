806d5094faf9e20b6884eb2a3e7b3fb6e1f8d168
abdolazizsalimi <aziz.s6882@gmail.com>
2022-03-29 09:22:20 +0430

    add: fix person info

--- nft_app/lib/models/profile.dart
+++ nft_app/lib/models/profile.dart
@@ -10,10 +10,11 @@ class Profile {
   static Profile generateProfile() {
     return Profile(
       imageUrl: 'imageUrl',
-      name: 'Abdolaziz',
-      email: 'email',
-      twitter: 'twitter',
-      description: 'description',
+      name: 'Abdolaziz Salimi',
+      email: 'aziz.s6882@gmail.com',
+      twitter: '@abdolazizsalimi',
+      description:
+          'A non-fungible token (NFT) is a non-interchangeable[clarification needed] unit of data stored on a blockchain, a form of digital ledger, that can be sold and traded. Types of NFT data units may be associated with digital files such as photos, videos, and audio. Because each token is uniquely identifiable, NFTs differ from blockchain cryptocurrencies, such as Bitcoin.',
     );
   }
 }
--- nft_app/lib/screens/profile/widgets/person_info.dart
+++ nft_app/lib/screens/profile/widgets/person_info.dart
@@ -18,7 +18,7 @@ class PersonInfo extends StatelessWidget {
             backgroundImage: NetworkImage(profile.imageUrl!),
             radius: 50,
           ),
-          const SizedBox(height: 30),
+          const SizedBox(height: 20),
           Text(
             profile.name!,
             style: const TextStyle(
@@ -26,6 +26,36 @@ class PersonInfo extends StatelessWidget {
               fontWeight: FontWeight.bold,
             ),
           ),
+          const SizedBox(height: 5),
+          Text(
+            profile.twitter!,
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+              color: Colors.blue,
+            ),
+          ),
+          const SizedBox(height: 20),
+          Text(
+            profile.description!,
+            style: const TextStyle(
+              fontSize: 14,
+              height: 1.5,
+              color: Colors.grey,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: 20),
+          Text(
+            profile.email!,
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+              height: 1.5,
+              color: Colors.black,
+            ),
+          ),
+          const SizedBox(height: 30),
         ],
       ),
     );
