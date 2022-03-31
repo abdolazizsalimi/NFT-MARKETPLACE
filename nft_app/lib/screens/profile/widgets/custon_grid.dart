24cd68a9c5e0b8263764731491c7b6d18fce4ecc
abdolazizsalimi <aziz.s6882@gmail.com>
2022-03-31 14:11:55 +0430

    add: detail screen

--- nft_app/lib/screens/profile/widgets/custon_grid.dart
+++ nft_app/lib/screens/profile/widgets/custon_grid.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:nft_app/models/art.dart';
 
+import '../../detail/widgets/detail.dart';
+
 class CustomGrid extends StatelessWidget {
   final String scrollkey;
   final List<Art> ArtList;
@@ -22,16 +24,25 @@ class CustomGrid extends StatelessWidget {
           crossAxisSpacing: 10,
           childAspectRatio: 4 / 5,
         ),
-        itemBuilder: (context, index) => Container(
-          height: 400,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-          ),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(10),
-            child: Image.network(
-              ArtList[index].imgUrl!,
-              fit: BoxFit.cover,
+        itemBuilder: (context, index) => GestureDetector(
+          onTap: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => DetailPage(),
+              ),
+            );
+          },
+          child: Container(
+            height: 400,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: Image.network(
+                ArtList[index].imgUrl!,
+                fit: BoxFit.cover,
+              ),
             ),
           ),
         ),
