c62b7246bc57ef63c5963ef11cf435702fdd4fd4
abdolazizsalimi <aziz.s6882@gmail.com>
2022-04-14 19:36:03 +0430

    change login and signup form and style

--- nft_app/lib/screens/auth/SignupForm.dart
+++ nft_app/lib/screens/auth/SignupForm.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:nft_app/screens/auth/LoginForm.dart';
-import 'package:nft_app/screens/profile/profile.dart';
 
 class SignupForm extends StatefulWidget {
   const SignupForm({Key? key}) : super(key: key);
@@ -10,251 +9,129 @@ class SignupForm extends StatefulWidget {
 }
 
 class _SignupFormState extends State<SignupForm> {
+  final TextEditingController _emailField = TextEditingController();
+  final TextEditingController _passwordField = TextEditingController();
+  final TextEditingController _confirmPasswordField = TextEditingController();
+  final TextEditingController _UserIdField = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        backgroundColor: Colors.pink[200],
-        shadowColor: Colors.pink[50],
-        elevation: 0,
-      ),
-      body: SingleChildScrollView(
-        scrollDirection: Axis.vertical,
-        child: Container(
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                const SizedBox(
-                  height: 30,
-                ),
-                const Text(
-                  'SignUp',
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black87,
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: const BoxDecoration(
+          color: Color.fromARGB(255, 255, 182, 193),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width / 1.3,
+              child: TextFormField(
+                style: const TextStyle(color: Colors.white),
+                controller: _emailField,
+                decoration: const InputDecoration(
+                  hintText: "something@email.com",
+                  hintStyle: TextStyle(
+                    color: Colors.white,
+                  ),
+                  labelText: "Email",
+                  labelStyle: TextStyle(
+                    color: Colors.white,
                   ),
                 ),
-                const SizedBox(
-                  height: 12,
-                ),
-                Image.asset(
-                  'assets/images/user.png',
-                  height: 100,
-                  width: 100,
-                ),
-                const Text(
-                  'Welcome !',
-                  style: TextStyle(
-                    height: 2,
-                    fontSize: 30,
-                    fontWeight: FontWeight.w500,
-                    color: Colors.black38,
-                  ),
-                ),
-                Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  margin: const EdgeInsets.only(top: 20),
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                      enabledBorder: const OutlineInputBorder(
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(30),
-                        ),
-                        borderSide: BorderSide(color: Colors.transparent),
-                      ),
-                      focusedBorder: const OutlineInputBorder(
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(30),
-                        ),
-                        borderSide: BorderSide(color: Colors.pink),
-                      ),
-                      prefixIcon: const Icon(
-                        Icons.person,
-                        color: Colors.black87,
-                      ),
-                      hintText: 'User ID',
-                      fillColor: Colors.pink[50],
-                      filled: true,
-                    ),
-                  ),
-                ),
-                Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  margin: const EdgeInsets.only(top: 12),
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                      enabledBorder: const OutlineInputBorder(
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(30),
-                        ),
-                        borderSide: BorderSide(color: Colors.transparent),
-                      ),
-                      focusedBorder: const OutlineInputBorder(
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(30),
-                        ),
-                        borderSide: BorderSide(color: Colors.pink),
-                      ),
-                      prefixIcon: const Icon(
-                        Icons.mail,
-                        color: Colors.black87,
-                      ),
-                      hintText: 'Email',
-                      fillColor: Colors.pink[50],
-                      filled: true,
-                    ),
-                  ),
-                ),
-                Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  margin: const EdgeInsets.only(top: 12),
-                  child: TextFormField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      enabledBorder: const OutlineInputBorder(
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(30),
-                        ),
-                        borderSide: BorderSide(color: Colors.transparent),
-                      ),
-                      focusedBorder: const OutlineInputBorder(
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(30),
-                        ),
-                        borderSide: BorderSide(color: Colors.pink),
-                      ),
-                      prefixIcon: const Icon(
-                        Icons.lock,
-                        color: Colors.black87,
-                      ),
-                      hintText: 'Password',
-                      fillColor: Colors.pink[50],
-                      filled: true,
-                    ),
-                  ),
-                ),
-                Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  margin: const EdgeInsets.only(top: 12),
-                  child: TextFormField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      enabledBorder: const OutlineInputBorder(
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(30),
-                        ),
-                        borderSide: BorderSide(color: Colors.transparent),
-                      ),
-                      focusedBorder: const OutlineInputBorder(
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(30),
-                        ),
-                        borderSide: BorderSide(color: Colors.pink),
-                      ),
-                      prefixIcon: const Icon(
-                        Icons.lock,
-                        color: Colors.black87,
-                      ),
-                      hintText: 'Confirm Password',
-                      fillColor: Colors.pink[50],
-                      filled: true,
-                    ),
-                  ),
-                ),
-                // Container(
-                //   margin: const EdgeInsets.all(20),
-                //   width: double.infinity,
-                //   height: 45,
-                //   padding: const EdgeInsets.symmetric(horizontal: 15),
-                //   child: RawMaterialButton(
-                //     fillColor: buttonColor,
-                //     shape: RoundedRectangleBorder(
-                //       borderRadius: BorderRadius.circular(15),
-                //     ),
-                //     elevation: 0,
-                //     child: const Text(
-                //       ' Login ',
-                //       style: TextStyle(
-                //         color: Colors.white,
-                //         fontWeight: FontWeight.bold,
-                //       ),
-                //     ),
-                //     onPressed: () {
-                //       //go to Profile
-                //       Navigator.push(
-                //         context,
-                //         MaterialPageRoute(builder: (context) => ProfilePage()),
-                //       );
-                //     },
-                //   ),
-                // ),
-                Container(
-                  margin: const EdgeInsets.all(20),
-                  width: double.infinity,
-                  height: 45,
-                  padding: const EdgeInsets.symmetric(horizontal: 15),
-                  child: RawMaterialButton(
-                    fillColor: Colors.white,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(15),
-                      side: const BorderSide(color: Colors.pink),
-                    ),
-                    elevation: 0,
-                    child: const Text(
-                      ' SignUp ',
-                      style: TextStyle(
-                        color: Colors.pink,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    onPressed: () {
-                      //go to Profile
-                      Navigator.pushAndRemoveUntil(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => ProfilePage(),
-                        ),
-                        (Route<dynamic> route) => false,
-                      );
-                    },
-                  ),
-                ),
-                Container(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Text(
-                        'Do you have an account? ',
-                        style: TextStyle(
-                          color: Colors.black87,
-                          fontSize: 16,
-                        ),
-                      ),
-                      FlatButton(
-                        textColor: Colors.pink,
-                        child: const Text(
-                          'Sign in',
-                        ),
-                        onPressed: () {
-                          //go to SignUp
-                          Navigator.pushAndRemoveUntil(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const LoginForm(),
-                            ),
-                            (Route<dynamic> route) => false,
-                          );
-                        },
-                      ),
-                    ],
-                  ),
-                ),
-              ],
+              ),
             ),
-          ),
+            SizedBox(height: MediaQuery.of(context).size.height / 35),
+            Container(
+              width: MediaQuery.of(context).size.width / 1.3,
+              child: TextFormField(
+                style: const TextStyle(color: Colors.white),
+                controller: _UserIdField,
+                decoration: const InputDecoration(
+                  hintText: "User ID",
+                  hintStyle: TextStyle(
+                    color: Colors.white,
+                  ),
+                  labelText: "User ID",
+                  labelStyle: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: MediaQuery.of(context).size.height / 35),
+            Container(
+              width: MediaQuery.of(context).size.width / 1.3,
+              child: TextFormField(
+                style: const TextStyle(color: Colors.white),
+                controller: _passwordField,
+                obscureText: true,
+                decoration: const InputDecoration(
+                  hintText: "password",
+                  hintStyle: TextStyle(
+                    color: Colors.white,
+                  ),
+                  labelText: "Password",
+                  labelStyle: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: MediaQuery.of(context).size.height / 35),
+            Container(
+              width: MediaQuery.of(context).size.width / 1.3,
+              child: TextFormField(
+                style: const TextStyle(color: Colors.white),
+                controller: _confirmPasswordField,
+                obscureText: true,
+                decoration: const InputDecoration(
+                  hintText: "Confirm password",
+                  hintStyle: TextStyle(
+                    color: Colors.white,
+                  ),
+                  labelText: "Password",
+                  labelStyle: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: MediaQuery.of(context).size.height / 35),
+            Container(
+              width: MediaQuery.of(context).size.width / 1.4,
+              height: 45,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15.0),
+                color: Colors.white,
+              ),
+              child: MaterialButton(
+                onPressed: () async {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const LoginForm(),
+                    ),
+                  );
+
+                  // bool shouldNavigate =
+                  //     await register(_emailField.text, _passwordField.text);
+                  // if (shouldNavigate) {
+                  //   Navigator.push(
+                  //     context,
+                  //     MaterialPageRoute(
+                  //       builder: (context) => HomeView(),
+                  //     ),
+                  //   );
+                  // }
+                },
+                child: const Text("Sign in"),
+              ),
+            ),
+            SizedBox(height: MediaQuery.of(context).size.height / 35),
+          ],
         ),
       ),
     );
